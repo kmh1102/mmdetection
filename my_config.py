97bc2584a1ebb897ca55da2def8c568a226a946d
M1102 <kmh1102@126.com>
2024-09-03 16:46:34 +0800

    自定义配置文件

---
+++ my_config.py
@@ -0,0 +1,64 @@
+from mmengine import Config
+from mmengine.runner import set_random_seed
+
+cfg = Config.fromfile('./configs/solov2/solov2_r50_fpn_1x_coco.py')
+
+# Modify dataset classes and color
+cfg.metainfo = {
+    'classes': ('FP',),
+    'palette': [
+        (220, 20, 60),
+    ]
+}
+
+# Modify dataset type and path
+cfg.data_root = '/data/home/user12/dl-projects/datasets/AAPD'
+
+cfg.train_dataloader.dataset.ann_file = 'annotations/train.json'
+cfg.train_dataloader.dataset.data_root = cfg.data_root
+cfg.train_dataloader.dataset.data_prefix.img = 'train/'
+cfg.train_dataloader.dataset.metainfo = cfg.metainfo
+
+cfg.val_dataloader.dataset.ann_file = 'annotations/val.json'
+cfg.val_dataloader.dataset.data_root = cfg.data_root
+cfg.val_dataloader.dataset.data_prefix.img = 'val/'
+cfg.val_dataloader.dataset.metainfo = cfg.metainfo
+
+cfg.test_dataloader = cfg.val_dataloader
+
+# Modify metric config
+cfg.val_evaluator.ann_file = cfg.data_root+'/'+'annotations/val.json'
+cfg.test_evaluator = cfg.val_evaluator
+
+# Modify num classes of the model in box head and mask head
+cfg.model.mask_head.num_classes = 1
+
+# We can still the pre-trained Mask RCNN model to obtain a higher performance
+cfg.load_from = 'checkpoints/solov2/solov2_x101_dcn_fpn_3x_coco_20220513_214337-aef41095.pth'
+
+# Set up working dir to save files and logs.
+# cfg.work_dir = './tutorial_exps'
+
+
+# We can set the evaluation interval to reduce the evaluation times
+cfg.train_cfg.val_interval = 1
+# We can set the checkpoint saving interval to reduce the storage cost
+cfg.default_hooks.checkpoint.interval = 1
+
+# The original learning rate (LR) is set for 8-GPU training.
+# We divide it by 8 since we only use one GPU.
+cfg.optim_wrapper.optimizer.lr = 0.02 / 10
+cfg.default_hooks.logger.interval = 10
+
+
+# Set seed thus the results are more reproducible
+# cfg.seed = 0
+set_random_seed(0, deterministic=False)
+
+# We can also use tensorboard to log the training process
+cfg.visualizer.vis_backends.append({"type":'TensorboardVisBackend'})
+
+#------------------------------------------------------
+config = f'./configs/solov2/solov2_r50_fpn_1x_apd.py'
+with open(config, 'w') as f:
+    f.write(cfg.pretty_text)
