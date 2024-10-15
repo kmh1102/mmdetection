e7896134df9ebd875b8d2099f8b5c656652d9610
M1102 <kmh1102@126.com>
2024-10-15 18:58:54 +0800

    MOD:添加了邮件提醒钩子

--- mmdet/engine/hooks/__init__.py
+++ mmdet/engine/hooks/__init__.py
@@ -11,11 +11,12 @@ from .visualization_hook import (DetVisualizationHook,
                                  GroundingVisualizationHook,
                                  TrackVisualizationHook)
 from .yolox_mode_switch_hook import YOLOXModeSwitchHook
+from .email_hook import EmailNotificationHook
 
 __all__ = [
     'YOLOXModeSwitchHook', 'SyncNormHook', 'CheckInvalidLossHook',
     'SetEpochInfoHook', 'MemoryProfilerHook', 'DetVisualizationHook',
     'NumClassCheckHook', 'MeanTeacherHook', 'trigger_visualization_hook',
     'PipelineSwitchHook', 'TrackVisualizationHook',
-    'GroundingVisualizationHook'
+    'GroundingVisualizationHook', 'EmailNotificationHook'
 ]
